d08e2719c461a49737633a4434764ac0c1d12c5b
JesuaneSaraivaVML <jesuane.saraiva@vml.com>
2024-10-24 15:49:50 +0100

    Implement Trending Section and fix pagination
    - logic fully implemented (i think)

--- js/app.js
+++ js/app.js
@@ -3,20 +3,29 @@
 import {
   URL_SEARCH,
   URL_RANDOM,
+  URL_TRENDING,
   ITEMS_PER_PAGE,
   MAX_API_LIMIT,
+  RATING,
 } from "./utils/constants.js";
 
 // DOM Elements
-const pictureElement = document.querySelector(".section-random__picture");
+const pictureElementRandom = document.querySelector(".section-random__picture");
 const nextRandomButton = document.querySelector(".section-random__next-btn");
 const searchForm = document.querySelector(".section-finder__form");
 const finderImageContainer = document.querySelector(".img-container--finder");
+const trendingImageContainer = document.querySelector(
+  ".img-container--trending"
+);
 const finderPaginationContainer = document.querySelector(
   ".section-finder__pagination"
 );
+const trendingPaginationContainer = document.querySelector(
+  ".section-trending__pagination"
+);
 
-let currentOffset = 0;
+let currentOffsetFinder = 0,
+  currentOffsetTrending = 0;
 
 function timeout(seconds) {
   return new Promise((_, reject) => {
@@ -25,41 +34,21 @@ function timeout(seconds) {
     }, seconds * 1000);
   });
 }
-
-async function search(query, limit = 8, offset = 0) {
+// Common fetch function for all API calls
+async function fetchFromAPI(url) {
   try {
     // Fetch from API
-    const response = await fetch(
-      `${URL_SEARCH}&q=${query}&limit=${limit}&offset=${offset}`
-    );
+    const response = await fetch(url);
     // Extract response
     const data = await response.json();
-
     //If request not successful, throw error
-    if (data.meta.status !== 200) throw new Error(JSON.stringify(data.meta));
-
+    if (data.meta.status !== 200) {
+      throw new Error(JSON.stringify(data.meta));
+    }
     // Return data
-    return { data: data.data, pagination: data.pagination };
+    return data;
   } catch (e) {
-    console.error("Error searching GIPHY");
-    throw e;
-  }
-}
-
-async function randomGIF(rating = "g") {
-  try {
-    // Fetch from API
-    const response = await fetch(`${URL_RANDOM}&rating=${rating}`);
-    // Extract response
-    const data = await response.json();
-
-    //If request not successful, throw error
-    if (data.meta.status !== 200) throw new Error(JSON.stringify(data.meta));
-
-    // Return data
-    return data.data;
-  } catch (e) {
-    console.error("Error getting random GIF");
+    console.error(`Error fetching from GIPHY: ${e.message}`);
     throw e;
   }
 }
@@ -67,8 +56,10 @@ async function randomGIF(rating = "g") {
 // TODO, to race with timeout, and then implement timer to send if it exceeds timeout
 async function handleLoadRandomGIF() {
   try {
-    const randomResult = await randomGIF();
-
+    const { data: randomResult } = await fetchFromAPI(
+      `${URL_RANDOM}&rating=${RATING}`
+    );
+    console.log(randomResult);
     // Get the media's URL from the Giphy API response for the original and mobile sizes
     const mediaUrlOriginal = randomResult.images.original.url;
     const mediaUrlSmall = randomResult.images.downsized.url;
@@ -78,37 +69,43 @@ async function handleLoadRandomGIF() {
     const mediaType = randomResult.type;
 
     // Update the <picture> element with the appropriate sources
-    pictureElement.innerHTML = `
+    pictureElementRandom.innerHTML = `
     <source srcset="${mediaUrlMedium}" media="(min-width: 768px)">
     <source srcset="${mediaUrlSmall}" media="(max-width: 599px)">
     <img class="section-random__image" src="${mediaUrlOriginal}" type="${mediaType}" alt="Random GIF">
   `;
   } catch (e) {
     // TODO improve this
-    pictureElement.innerHTML = `
+    pictureElementRandom.innerHTML = `
     Error fetching random GIF
   `;
   }
 }
 
 // Function to update pagination controls
-function updatePaginationControls(pagination, query) {
-  const total =
-    pagination.total_count > MAX_API_LIMIT
-      ? MAX_API_LIMIT
-      : pagination.total_count;
+function updatePaginationControls(pagination, section) {
+  const total = Math.min(pagination.total_count, MAX_API_LIMIT);
+  const offsets = {
+    finder: currentOffsetFinder,
+    trending: currentOffsetTrending,
+  };
+  const containers = {
+    finder: finderPaginationContainer,
+    trending: trendingPaginationContainer,
+  };
+
+  const currentOffset = offsets[section];
   const totalPages = Math.ceil(total / ITEMS_PER_PAGE);
   const currentPage = Math.floor(currentOffset / ITEMS_PER_PAGE) + 1;
 
   // Create/Update pagination btns
   const paginationHTML = `
-      <div class="pagination section-finder__pagination">
           <button
             class="pagination__button" ${currentPage === 1 ? "disabled" : ""} 
           data-action="prev">
             <i class="pagination__icon fa-solid fa-caret-left"></i>
           </button>
-          <span class="pagination__number section-finder__pagination-number"
+          <span class="pagination__number section-${section}__pagination-number"
             >${currentPage} of ${totalPages}</span
           >
           <button
@@ -116,21 +113,34 @@ function updatePaginationControls(pagination, query) {
           ${currentPage === totalPages ? "disabled" : ""} data-action="next">
             <i class="pagination__icon fa-solid fa-caret-right"></i>
           </button>
-        </div>
     `;
-  finderPaginationContainer.innerHTML = paginationHTML;
-  const paginationButtons = finderPaginationContainer.querySelectorAll(
+  const paginationContainer = containers[section];
+  paginationContainer.innerHTML = paginationHTML;
+  const paginationButtons = paginationContainer.querySelectorAll(
     ".pagination__button"
   );
   paginationButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
       const action = btn.dataset.action;
-      if (action === "prev" && currentOffset >= ITEMS_PER_PAGE) {
-        currentOffset -= ITEMS_PER_PAGE;
-      } else if (action === "next" && currentOffset + ITEMS_PER_PAGE < total) {
-        currentOffset += ITEMS_PER_PAGE;
+
+      // Update offset based on action
+      if (action === "prev" && offsets[section] >= ITEMS_PER_PAGE) {
+        offsets[section] -= ITEMS_PER_PAGE;
+      } else if (
+        action === "next" &&
+        offsets[section] + ITEMS_PER_PAGE < total
+      ) {
+        offsets[section] += ITEMS_PER_PAGE;
       }
-      handleSearch(null, currentOffset);
+
+      // Update the corresponding offset variable and call handler
+      if (section === "finder") {
+        currentOffsetFinder = offsets[section];
+        handleSearch(null, currentOffsetFinder);
+      } else {
+        currentOffsetTrending = offsets[section];
+        handleTrendingGIFs(currentOffsetTrending);
+      }
     });
   });
 }
@@ -139,14 +149,14 @@ function updatePaginationControls(pagination, query) {
 async function handleSearch(e, offset = 0) {
   e?.preventDefault(); // making it optional so we can call this function directly for pagination!!!! IMPORTANT TO KNOW
 
-  const form =
-    e?.currentTarget || document.querySelector(".section-finder__form");
+  const form = document.querySelector(".section-finder__form");
   const formData = new FormData(form);
   const query = formData.get("section-finder__input");
 
   try {
-    const result = await search(query, ITEMS_PER_PAGE, offset);
-    const { data, pagination } = result;
+    const { data, pagination } = await fetchFromAPI(
+      `${URL_SEARCH}&q=${query}&limit=${ITEMS_PER_PAGE}&offset=${offset}`
+    );
 
     // Reset image container
     finderImageContainer.innerHTML = "";
@@ -171,7 +181,43 @@ async function handleSearch(e, offset = 0) {
     });
 
     // Update pagination controls
-    updatePaginationControls(pagination, query);
+    updatePaginationControls(pagination, "finder");
+  } catch (e) {
+    // TODO
+    console.log(e);
+  }
+}
+
+async function handleTrendingGIFs(offset = 0) {
+  try {
+    const { data, pagination } = await fetchFromAPI(
+      `${URL_TRENDING}&rating=${RATING}&limit=${ITEMS_PER_PAGE}&offset=${offset}`
+    );
+
+    // Reset image container
+    trendingImageContainer.innerHTML = "";
+
+    // Insert images
+    data.forEach((img) => {
+      const mediaUrlOriginal = img.images?.original?.url;
+      const mediaUrlSmall = img.images?.downsized?.url;
+      const mediaUrlMedium = img.images?.downsized_medium?.url;
+      const mediaType = img.type;
+      const alt = img.title;
+
+      trendingImageContainer.insertAdjacentHTML(
+        "beforeend",
+        `            
+        <picture class="img-container__picture">
+          <source srcset="${mediaUrlMedium}" media="(min-width: 768px)">
+          <source srcset="${mediaUrlSmall}" media="(max-width: 599px)">
+          <img class="img-container__img" src="${mediaUrlOriginal}" type="${mediaType}" alt="${alt}">
+        </picture>`
+      );
+    });
+
+    // Update pagination controls
+    updatePaginationControls(pagination, "trending");
   } catch (e) {
     // TODO
     console.log(e);
@@ -191,4 +237,5 @@ function init() {
 // Initialize app and Setup necessary event listeners
 init();
 
-// window.addEventListener("load", handleLoadRandomGIF);
+window.addEventListener("load", handleLoadRandomGIF);
+window.addEventListener("load", handleTrendingGIFs);
--- js/utils/constants.js
+++ js/utils/constants.js
@@ -2,6 +2,8 @@ import { GIPHY_API_KEY } from "./config.js";
 
 export const URL_SEARCH = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}`;
 export const URL_RANDOM = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}`;
+export const URL_TRENDING = `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`;
 
 export const ITEMS_PER_PAGE = 8;
 export const MAX_API_LIMIT = 50;
+export const RATING = "g";
