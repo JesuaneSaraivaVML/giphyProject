ce1f712efab4bc986b30ec6f10c3bb28f0bcbde3
JesuaneSaraivaVML <jesuane.saraiva@vml.com>
2024-10-24 14:36:30 +0100

    Implement pagination controls in finder section

--- js/app.js
+++ js/app.js
@@ -1,12 +1,22 @@
 "use strict";
 
-import { URL_SEARCH, URL_RANDOM } from "./utils/constants.js";
+import {
+  URL_SEARCH,
+  URL_RANDOM,
+  ITEMS_PER_PAGE,
+  MAX_API_LIMIT,
+} from "./utils/constants.js";
 
 // DOM Elements
 const pictureElement = document.querySelector(".section-random__picture");
 const nextRandomButton = document.querySelector(".section-random__next-btn");
 const searchForm = document.querySelector(".section-finder__form");
 const finderImageContainer = document.querySelector(".img-container--finder");
+const finderPaginationContainer = document.querySelector(
+  ".section-finder__pagination"
+);
+
+let currentOffset = 0;
 
 function timeout(seconds) {
   return new Promise((_, reject) => {
@@ -81,28 +91,74 @@ async function handleLoadRandomGIF() {
   }
 }
 
-// TODO, to race with timeout, and then implement timer to send if it exceeds timeout
-async function handleSearch(e) {
-  e.preventDefault();
+// Function to update pagination controls
+function updatePaginationControls(pagination, query) {
+  const total =
+    pagination.total_count > MAX_API_LIMIT
+      ? MAX_API_LIMIT
+      : pagination.total_count;
+  const totalPages = Math.ceil(total / ITEMS_PER_PAGE);
+  const currentPage = Math.floor(currentOffset / ITEMS_PER_PAGE) + 1;
 
-  const form = e.currentTarget;
+  // Create/Update pagination btns
+  const paginationHTML = `
+      <div class="pagination section-finder__pagination">
+          <button
+            class="pagination__button" ${currentPage === 1 ? "disabled" : ""} 
+          data-action="prev">
+            <i class="pagination__icon fa-solid fa-caret-left"></i>
+          </button>
+          <span class="pagination__number section-finder__pagination-number"
+            >${currentPage} of ${totalPages}</span
+          >
+          <button
+            class="pagination__button"
+          ${currentPage === totalPages ? "disabled" : ""} data-action="next">
+            <i class="pagination__icon fa-solid fa-caret-right"></i>
+          </button>
+        </div>
+    `;
+  finderPaginationContainer.innerHTML = paginationHTML;
+  const paginationButtons = finderPaginationContainer.querySelectorAll(
+    ".pagination__button"
+  );
+  paginationButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const action = btn.dataset.action;
+      if (action === "prev" && currentOffset >= ITEMS_PER_PAGE) {
+        currentOffset -= ITEMS_PER_PAGE;
+      } else if (action === "next" && currentOffset + ITEMS_PER_PAGE < total) {
+        currentOffset += ITEMS_PER_PAGE;
+      }
+      handleSearch(null, currentOffset);
+    });
+  });
+}
+
+// TODO, to race with timeout, and then implement timer to send if it exceeds timeout
+async function handleSearch(e, offset = 0) {
+  e?.preventDefault(); // making it optional so we can call this function directly for pagination!!!! IMPORTANT TO KNOW
+
+  const form =
+    e?.currentTarget || document.querySelector(".section-finder__form");
   const formData = new FormData(form);
   const query = formData.get("section-finder__input");
 
   try {
-    const result = await search(query);
-    const data = result.data;
+    const result = await search(query, ITEMS_PER_PAGE, offset);
+    const { data, pagination } = result;
 
     // Reset image container
     finderImageContainer.innerHTML = "";
 
+    // Insert images
     data.forEach((img) => {
       const mediaUrlOriginal = img.images?.original?.url;
       const mediaUrlSmall = img.images?.downsized?.url;
       const mediaUrlMedium = img.images?.downsized_medium?.url;
       const mediaType = img.type;
       const alt = img.title;
-      console.log(img);
+
       finderImageContainer.insertAdjacentHTML(
         "beforeend",
         `            
@@ -113,17 +169,18 @@ async function handleSearch(e) {
         </picture>`
       );
     });
+
+    // Update pagination controls
+    updatePaginationControls(pagination, query);
   } catch (e) {
     // TODO
     console.log(e);
   }
 }
 
+// Function to add even listeners
 function addEventListeners() {
-  // To the random section next button
   nextRandomButton.addEventListener("click", handleLoadRandomGIF);
-
-  // To the search form
   searchForm.addEventListener("submit", handleSearch);
 }
 
